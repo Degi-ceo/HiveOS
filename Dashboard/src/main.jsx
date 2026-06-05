beea5b54d17fd1e659df6bd95f167a64cc5e28cc
hiveOSagent <hive.os.agent@gmail.com>
2026-06-05 09:57:07 +0100

    Add files via upload

---
+++ Dashboard/src/main.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import MissionControl from "../MissionControl.jsx";
+createRoot(document.getElementById("root")).render(<MissionControl />);
